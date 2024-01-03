825fc901a9a96b39ed8e03de87397f6c1fb8652c
ardamerttarkan <arda.tarkan60@hotmail.com>
2024-01-03 16:21:07 +0300

    add index

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const MerhabaTitle = document.querySelector(".MerhabaTitle");
 const OdinTitle = document.querySelector(".OdinTitle");
 const KayboldumTitle = document.querySelector(".KayboldumTitle");
 const AciklamaTitle = document.querySelector(".AciklamaTitle");
+const btnCall = document.querySelector(".btnCall");
 
 
 buttons.forEach((button) => {
@@ -14,6 +15,7 @@ buttons.forEach((button) => {
         OdinTitle.innerHTML = data[lang].OdinTitle;
         KayboldumTitle.innerHTML = data[lang].KayboldumTitle;
         AciklamaTitle.innerHTML = data[lang].AciklamaTitle;
+        btnCall.innerHTML = data[lang].btnCall;
     });
 });
 
@@ -27,6 +29,7 @@ const data = {
     "OdinTitle": "Odin",
     "KayboldumTitle": "Sanırım Kayboldum 😿",
     "AciklamaTitle": "Lütfen Sahibime Ulaşmama Yardım Eder Misiniz? Kendisi Çok Endişelenmiş Olmalı.<br> Ona Aşağıdaki Butonlardan Ulaşabilirsiniz. Yardımınız İçin Teşekkür Ederiz.",
+    "btnCall": "Ara Beni"
     },
 
     "english":
@@ -35,7 +38,9 @@ const data = {
     "OdinTitle": "Odin",
     "KayboldumTitle": "I Think I'm Lost 😿",
     "AciklamaTitle": "Can You Please Help Me Reach My Owner? He Must Be Very Worried.<br> You Can Reach Him Using the Buttons Below. Thank You For Your Help.",
+    "btnCall": "Call Me"
     },
+
 
     "arabic":
     {
@@ -43,6 +48,15 @@ const data = {
         "OdinTitle": "Odin",
         "KayboldumTitle": "أعتقد أنني ضعت 😿",
         "AciklamaTitle": "هل يمكنك مساعدتي في الوصول إلى مالكي؟ لا بد أنه قلق جدًا.<br> يمكنك الوصول إليه باستخدام الأزرار الموجودة أدناه. شكرا لك على مساعدتك.",
-        }
+        "btnCall": "اتصل بي"
+    ,}
 
+}
+
+function gotoWhatsapp(link){
+    window.open(link.value);
+}
+
+function CallMe(){
+    window.open("tel:+9005432014890");
 }
